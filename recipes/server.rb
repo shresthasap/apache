29b7a1f63ebd2654afbe1808dd9d12a81c3b6d61
vagrant <you@example.com>
2018-02-24 23:48:52 +0000

    refactor web app with template

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,11 +6,8 @@
 
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-  <h2>IPADDRESS: #{node['ipaddress']}</h2>
-  <h2>HOSTNAME: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.erb'
 end
 
 service 'httpd' do
